71cbea043be054210137229a1aa0e209ff724eee
Rafael Rojas <yugnarcorp@hotmail.com>
2021-11-23 22:12:46 -0600

    Stuff

---
+++ backend/emailSendTestScript.js
@@ -0,0 +1,17 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('SG.osYexCuWQTeygC0C2GYECQ.ySHAOYc_TsbeCdIIrjB6ZdWQaCmxuXxAIX0zSa74xdQ');
+const msg = {
+  to: 'yugnar@outlook.com',
+  from: 'A01339605@tec.mx',
+  subject: 'SendGrid test Number 1',
+  text: 'Test of text field, we will also include some <p>html tags here just like a good old <br> linebreak ooo yeah </p>',
+  html: 'Also testing the html tag, with more html tags: <strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })

7c5f29e71505dee838ba4c32e405b0613e99bc6e
Alejandro López Martínez <alejandrolm14@gmail.com>
2021-11-24 00:36:29 -0600

    Added next

---
+++ front/pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
